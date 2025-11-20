f5bee26e84ee2f8f1709b0bca554c399505ed6d6
SAYAH Abdelhadi <your.email@example.com>
2025-11-20 16:00:37 +0100

    exo3 est fait

--- src/main/java/com/polytech/tp/CoursEnLigne.java
+++ src/main/java/com/polytech/tp/CoursEnLigne.java
@@ -2,20 +2,16 @@ package com.polytech.tp;
 
 public class CoursEnLigne extends CoursDecorator {
     CoursEnLigne(ICours cours) {
-        //TODO: Implémenter le reste ...
+        super(cours);
     }
 
     @Override
     public String getDescription() {
-        // TODO Auto-generated method stub
-        
-        return null;
+        return coursDecorated.getDescription() + " (En ligne)";
     }
 
     @Override
     public double getDuree() {
-        // TODO Auto-generated method stub
-        
-        return 0;
+       return coursDecorated.getDuree();
     };
 }
